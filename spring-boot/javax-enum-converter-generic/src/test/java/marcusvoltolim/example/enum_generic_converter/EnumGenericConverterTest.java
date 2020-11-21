e02971110e05fc590b5565ed488e22dcacabb766
Marcus Neves Satelis <marcus.neves.satelis@everis.com>
2020-11-21 20:44:06 -0300

    improves tests

--- spring-boot/javax-enum-converter-generic/src/test/java/marcusvoltolim/example/enum_generic_converter/EnumGenericConverterTest.java
+++ spring-boot/javax-enum-converter-generic/src/test/java/marcusvoltolim/example/enum_generic_converter/EnumGenericConverterTest.java
@@ -1,14 +1,20 @@
 package marcusvoltolim.example.enum_generic_converter;
 
+import marcusvoltolim.example.enum_generic_converter.enums.EnumsConverters;
 import marcusvoltolim.example.enum_generic_converter.enums.UserType;
 import marcusvoltolim.example.enum_generic_converter.model.User;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import static marcusvoltolim.example.enum_generic_converter.enums.UserType.ADMIN;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @DataJpaTest
 class EnumGenericConverterTest {
@@ -18,46 +24,56 @@ class EnumGenericConverterTest {
 
 	@Test
 	void usingEnumConverterGeneric() {
-		var id = testEntityManager.persistAndGetId(new User(UserType.ADMIN));
+		var id = testEntityManager.persistAndGetId(new User(ADMIN));
 		var typeBd = testEntityManager.getEntityManager()
 									  .createNativeQuery("SELECT user_type_converter FROM User WHERE id = :id")
 									  .setParameter("id", id)
 									  .getSingleResult();
 
 		assertThat(typeBd)
-			.isEqualTo(UserType.ADMIN.getId())
-			.isNotEqualTo(UserType.ADMIN.ordinal())
-			.isNotEqualTo(UserType.ADMIN.name());
+			.isEqualTo(ADMIN.getId())
+			.isNotEqualTo(ADMIN.ordinal())
+			.isNotEqualTo(ADMIN.name());
 	}
-
 
 	@Test
 	void usingOrdinalValue() {
-		var id = testEntityManager.persist(new User(UserType.ADMIN));
+		var id = testEntityManager.persist(new User(ADMIN));
 		var typeBd = testEntityManager.getEntityManager()
 									  .createNativeQuery("SELECT user_type_ordinal FROM User WHERE id = :id")
 									  .setParameter("id", id)
 									  .getSingleResult();
 
 		assertThat(typeBd)
-			.isNotEqualTo(UserType.ADMIN.getId())
-			.isEqualTo(UserType.ADMIN.ordinal())
-			.isNotEqualTo(UserType.ADMIN.name());
+			.isNotEqualTo(ADMIN.getId())
+			.isEqualTo(ADMIN.ordinal())
+			.isNotEqualTo(ADMIN.name());
 
 	}
 
 	@Test
 	void usingStringValue() {
-		var id = testEntityManager.persistAndGetId(new User(UserType.ADMIN));
+		var id = testEntityManager.persistAndGetId(new User(ADMIN));
 		var typeBd = testEntityManager.getEntityManager()
 									  .createNativeQuery("SELECT user_type_string FROM User WHERE id = :id")
 									  .setParameter("id", id)
 									  .getSingleResult();
 
 		assertThat(typeBd)
-			.isNotEqualTo(UserType.ADMIN.getId())
-			.isNotEqualTo(UserType.ADMIN.ordinal())
-			.isEqualTo(UserType.ADMIN.name());
+			.isNotEqualTo(ADMIN.getId())
+			.isNotEqualTo(ADMIN.ordinal())
+			.isEqualTo(ADMIN.name());
+	}
+
+	@Test
+	void nullValue() {
+		assertNull(EnumsConverters.AbstractPersistableEnumConverter.fromId(null, UserType.values()));
+	}
+
+	@Test
+	void values() {
+		var values = UserType.values();
+		Arrays.stream(values).forEach(value -> assertEquals(value, EnumsConverters.AbstractPersistableEnumConverter.fromId(value.getId(), values)));
 	}
 
 }
